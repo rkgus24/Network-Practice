da2137a1b882f952729db0f9b31d87e834297c62
Kim-Gangmin <101308701+min-524@users.noreply.github.com>
2022-12-13 17:52:09 +0900

    Upload :: 골프장

---
+++ workspace01/HRD_0000/src/main/webapp/src/main/webapp/check.js
@@ -0,0 +1,36 @@
+let memberNumber
+function checking(){
+	if(frm.date.value.length === 0) {
+		alert("수강일이 입력되지 않았습니다.")
+		frm.date.focus()
+		return false
+	} else if(frm.memberName.value.length === 0){
+		alert("회원명이 입력되지 않았습니다.")
+		frm.memberName.focus()
+		return false
+	} else if(frm.area.value.length === 0){
+		alert("강의장소이 입력되지 않았습니다.")
+		frm.area.focus()
+		return false
+	} else if(frm.className.value.length === 0){
+		alert("강의명이 입력되지 않았습니다.")
+		frm.className.focus()
+		return false
+	} 
+	alert("수강신청이 정상적으로 완료되었습니다!")
+	document.frm.sumbit()
+	return true
+}
+
+function getNumber(number){
+	memberNumber = number
+	frm.number.value = number
+}
+function reseting(){
+	alert("정보를 지우고 처음부터 다시 입력합니다!")
+	document.frm.reset()
+}
+function getPrice(price){
+	if(memberNumber >= 20000 ) frm.price.value = price / 2
+	else frm.price.value = price
+}
